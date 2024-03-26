568390643737b93688e4f7c949f9264315096364
ANTHONY CHAMPION <anthonyrodrigues.c@gmail.com>
2024-03-26 16:53:09 +0100

    step 4 done with mew

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,26 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png">
-        </img>
-        <figcaption>Bulbasaur</figcaption>
-    </figure>;
-}
+    const pokemon = pokemonList[1];
+
+    return (
+        <figure>
+            {pokemon.imgSrc === undefined ? <div> <p> ???</p><figcaption>{pokemon.name}  </figcaption></div> : <div><img src={pokemon.imgSrc} /> <figcaption>{pokemon.name}</figcaption></div>}
+        </figure >
+    );
+};
+
+
+
+
 
 export default PokemonCard;
